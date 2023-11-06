2db23ad117ed440b00e45e1e1bdd843ad41d04c7
bEgemot32015 <hipomotgames@gmail.com>
2023-11-06 14:46:27 +0200

    Redis Enabled

--- medusa-config.js
+++ medusa-config.js
@@ -58,7 +58,7 @@ const plugins = [
 ];
 
 const modules = {
-  /*eventBus: {
+  eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
       redisUrl: REDIS_URL
@@ -69,7 +69,7 @@ const modules = {
     options: {
       redisUrl: REDIS_URL
     }
-  },*/
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
